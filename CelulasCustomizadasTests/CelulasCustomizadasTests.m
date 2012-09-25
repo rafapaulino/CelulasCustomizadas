0f72693f1a9596d50a807cdb16bc6ecef8da47b4
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-09-25 09:32:05 -0300

    Initial Commit

---
+++ CelulasCustomizadasTests/CelulasCustomizadasTests.m
@@ -0,0 +1,32 @@
+//
+//  CelulasCustomizadasTests.m
+//  CelulasCustomizadasTests
+//
+//  Created by Rafael Brigagão Paulino on 25/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "CelulasCustomizadasTests.h"
+
+@implementation CelulasCustomizadasTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CelulasCustomizadasTests");
+}
+
+@end
